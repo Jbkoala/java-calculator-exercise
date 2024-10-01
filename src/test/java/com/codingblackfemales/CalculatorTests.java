fc51cf600eb0682f846a39992996f7355df3e11d
Jbkoala <jbailey0233@gmail.com>
2024-10-01 21:26:51 +0100

    I have added scenarios for multiply and divide methods. Do need to add more scenarios.

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName(value = "The Calculator should")
 public class CalculatorTests {
@@ -23,4 +24,41 @@ public class CalculatorTests {
 
         assertEquals(-2, difference);
     }
+
+    @Test
+    @DisplayName("multiply two numbers")
+    public  void testMultiplication(){
+    final Integer multiply = Calculator.multiply(3,5);
+    assertEquals(15, multiply);
+    }
+
+    @Test
+    @DisplayName("Multiply a negative number with a positive number")
+    public void test1NegNumMultiplied(){
+        final Integer multiplyAgainstANeg = Calculator.multiply(-2,5);
+        assertEquals(-10,multiplyAgainstANeg);
+    }
+
+    @Test
+    @DisplayName("Multiply a negative number with another negative number")
+    public void test2NegNumMultiplied(){
+        final Integer multiplyAgainstNegs = Calculator.multiply(-2,-5);
+        assertEquals(-10,multiplyAgainstNegs);
+    }
+
+    @Test
+    @DisplayName("Divide two numbers")
+    public void testDivide(){
+      final Integer divide = Calculator.divide(2,2);
+      assertEquals(1, divide);
+    }
+
+    @Test
+    @DisplayName("Divide by zero")
+    public void testDividebyZero(){
+        final Integer dividebyZero = Calculator.divide(2,0);
+//        assertThrows(ArithmeticException.class, ()-> 0, dividebyZero);
+    }
+
+
 }
